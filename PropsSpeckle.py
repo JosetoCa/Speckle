33c4d0067a94db63ab49f380fa0bd7b5b6261283
JosetoCa <jose.camacho1@udea.edu.co>
2025-03-18 16:47:46 -0500

    Cambio A funciones
    Definí funciones para obtener solo un parámetro estadístico, incluí el contraste y ahora el nombre de la imagen es una entrada de la función imagen, para no tener que entregar un input.

--- PropsSpeckle.py
+++ PropsSpeckle.py
@@ -48,17 +48,23 @@ class PropsSpeckle:
             print(f"Ocurrió un error al leer el archivo: {e}")
             self.primera_linea = None
             sys.exit()
+        # Inicialización de variables
+        self.spec = None
+        self.media = None
+        self.desviacion = None
+        self.varianza = None
+        self.constraste = None
+
 
 
 
 ## Métodos de la clase PropsSpeckle ##
 
 # Presentación en pantalla de la imagen
-    def imagen(self, show=True):
+    def imagen(self, nombre:str, show=True):
         # Carga y muestra una imagen en escala de grises desde la ruta 
         # almacenada en el archivo de texto.
         try:
-            nombre = input("Ingrese el nombre de la imagen con su extensión: ")
             ruta_imagen = self.primera_linea + '\\' + nombre 
             #Imagen en escala de grises
             imagen = Image.open(ruta_imagen).convert('L') 
@@ -76,19 +82,59 @@ class PropsSpeckle:
 
 
 # Cálculo de parámetros estadísticos
-    def statistics(self):
+    def statisticspro(self):
 
         # Calcula la media, varianza y la desviación estándar de la imagen
         self.media = np.mean(self.spec)
         self.desviacion = np.std(self.spec)
         self.varianza = np.var(self.spec)
+        self.constraste = self.desviacion / self.media
 
-        logging.info(f"Se ejecuta el método statistics")
+        logging.info(f"Se ejecuta el método statisticspro")
         logging.info(f"Valor promedio de la imagen = {self.media}")
         logging.info(f"Valor desviación estándar de la imagen {self.desviacion}")
         logging.info(f"Valor varianza de la imagen {self.varianza}")
+        logging.info(f"Valor contraste de la imagen {self.constraste}")
+
+        print(f"Valor de la intensidad media es = {self.media}")
+        print(f"Valor de la desviación estándar es = {self.desviacion}")
+        print(f"Valor de la varianza es = {self.varianza}")
+        print(f"Valor del contraste es = {self.constraste}")
+
+        plt.hist(self.spec.flatten(), bins=256, density=True)
+        plt.title('Histograma de la imagen')
+        plt.xlabel('Niveles de gris')
+        plt.ylabel('Frecuencia')
+        plt.show()
 
 
+    def cal_media(self):
+        # Calcula la media de la imagen
+        self.media = np.mean(self.spec)
+        logging.info(f"Se ejecuta el método cal_media")
+        logging.info(f"Valor promedio de la imagen = {self.media}")
+        return self.media
+    
+    def cal_desviacion(self):
+        # Calcula la desviación estándar de la imagen
+        self.desviacion = np.std(self.spec)
+        logging.info(f"Se ejecuta el método cal_desviacion")
+        logging.info(f"Valor desviación estándar de la imagen {self.desviacion}")
+        return self.desviacion
+    
+    def cal_varianza(self):
+        # Calcula la varianza de la imagen
+        self.varianza = np.var(self.spec)
+        logging.info(f"Se ejecuta el método cal_varianza")
+        logging.info(f"Valor varianza de la imagen {self.varianza}")
+        return self.varianza
+    
+    def cal_contraste(self):
+        # Calcula el contraste de la imagen
+        self.constraste = self.desviacion / self.media
+        logging.info(f"Se ejecuta el método cal_contraste")
+        logging.info(f"Valor contraste de la imagen {self.constraste}")
+        return self.constraste
 
 # Normalización de la imagen
     def normalizar(self):
@@ -98,8 +144,13 @@ class PropsSpeckle:
         self.media = np.mean(self.spec)
         self.desviacion = np.std(self.spec)
         self.varianza = np.var(self.spec)
-
-
+        self.constraste = self.desviacion / self.media
+        logging.info(f"Se ejecuta el método normalizar")
+        logging.info(f"Valor promedio de la imagen = {self.media}")
+        logging.info(f"Valor desviación estándar de la imagen {self.desviacion}")
+        logging.info(f"Valor varianza de la imagen {self.varianza}")
+        logging.info(f"Valor contraste de la imagen {self.constraste}")
+        
 # Histograma de la imagen
     def histograma(self):
         # Genera un histograma de la imagen
