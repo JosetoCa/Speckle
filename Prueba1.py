9d391df347955f8d4971577cd587b11295fc0ed4
Edgar Rueda <edgar.rueda@udea.edu.co>
2025-03-20 10:50:34 -0500

    Varios cambios
    PropsSpeckle:
    - Ahora, si el usuario no pone el nombre de la imagen, se le pide que lo ingrese por la consola.
    - Eliminé la impresión del histograma de statisticspro, para que solo se haga llamando la función histograma
    - Eliminé el cálculo de los parámetros estadísticos de normalizar para que sea sólo una función de normalizar

--- Prueba1.py
+++ Prueba1.py
@@ -2,26 +2,27 @@ import PropsSpeckle as ps
 
 import os
 
+## Para verificar el directorio de trabajo, donde se guarda bitaora.txt
 print("Directorio actual:", os.getcwd())
 
 # Import the PropsSpeckle module
 espacio = ps.PropsSpeckle()
-
+# Parámetros del módulo
 print('Versión:', espacio.version)
 print('Nombre:', espacio.name)
 print('Ruta',espacio.primera_linea)  # None
 
-espacio.imagen('4-3.tif', show=True)
-
+#Se llama una imagen, en este caso se da el nombre
+espacio.imagen('4-3.tif',show=True)
+# Información de estadística primer orden
 espacio.statisticspro()
-
-print('Parámetros estadísticos de la imagen:')
-print('Media:', espacio.media)
-print('Desviación estándar:', espacio.desviacion)  
-print('Varianza:', espacio.varianza) 
+# Se normaliza la imagen
 espacio.normalizar()
-print('Parámetros estadísticos de la imagen normalizada:')
-print('Media:', espacio.media)
-print('Desviación estándar:', espacio.desviacion)  
-print('Varianza:', espacio.varianza) 
+#Se vuelven a determinar los parámetros estadísticos de primer orden
+espacio.statisticspro()
+#Grafica el histograma
+espacio.histograma()
+
+#Se llama otra imagen, sin dar el nombre de la imagen,se grafica histograma
+espacio.imagen(show=None)
 espacio.histograma()
