012a588c886e15206277cfec5514d4db2ff9259a
Edgar Rueda <elgaral@gmail.com>
2025-06-13 16:35:36 -0500

    bitacora

--- Prueba4_SimilitudCoseno.py
+++ Prueba4_SimilitudCoseno.py
@@ -5,6 +5,7 @@ import PropsSpeckle as ps
 espacio2 = ps.PropsSpeckle()
 # Carga la imagen y calcula estadísticas y autocorrelación
 espacio2.imagen('4-150-1.tif',show=False)
+espacio2.modificar()
 espacio2.statisticspro()
 # Calcula la similitud coseno entre las imágenes
-espacio2.histograma_similaridad()
+espacio2.histograma_similaridad(imagen='m')
